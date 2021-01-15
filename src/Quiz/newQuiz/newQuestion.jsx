209de03666fcf2731e71f1822357772582e77bd7
Euro28 <eurol197628@gmail.com>
2021-01-15 17:11:18 +0000

    splitting components

--- src/Quiz/newQuiz/newQuestion.jsx
+++ src/Quiz/newQuiz/newQuestion.jsx
@@ -4,6 +4,8 @@ import { Form, Button } from "react-bootstrap";
 import axios from "axios";
 
 import NewAnswer from "./newAnswer.jsx";
+import EnteredAnswers from "./enteredAnswers.jsx";
+import CurrentQuestions from "./currentQuestions.jsx";
 //the form you have two initial fields,
 //first is the question name
 //the second is a button which says add a new answer
@@ -12,23 +14,10 @@ import NewAnswer from "./newAnswer.jsx";
 //the answer form with 3 fields shows up, when you click submit answer
 //it submits answer to answers variable and sets form answer form to not visible again
 //
-//the form for adding a new answer should have 3 fields
-//the first field is display answer
-//the second field should be a dropdown of concepts, if the concepts
-//they are looking for is not found they should be able to add a custom one that is saved
-//the third field is a checkbox stating the answer is correct
-//
 //if the user types in misconception field the correct checkbox should be disabled
 //if the user ticks the correct checkbox the misconception field should be cleared and
 //disabled
-//
-//after submitting the question, there should be at a minimum two answers
-//and at least one answer must be marked correct
 
-//each answer prinnted should also have a remove and edit button
-//remove button is simple remove from object
-//edit will open newAnswer with passed in current props, and delete current entry.
-//
 const NewQuestion = () => {
   const [answers, setAnswers] = useState([]);
   const [title, setTitle] = useState("");
@@ -48,7 +37,6 @@ const NewQuestion = () => {
     const getQuiz = async () => {
       const quizQuestions = await axios.get("/api/questions");
       const questions = quizQuestions.data.pages[0].elements;
-      console.log(questions);
       setQuestions(questions);
     };
     getQuiz();
@@ -102,29 +90,9 @@ const NewQuestion = () => {
           )}
         </MYFORM>
         {newAnswer && <NewAnswer addAnswer={addAnswer} />}
-        {answers.map((answer) => (
-          <li key={answer.text}>
-            {answer.value} {answer.text} {String(answer.correct)}
-          </li>
-        ))}
+        <EnteredAnswers answers={answers} />
       </CONTAINER>
-      <ul>
-        {questions &&
-          questions
-            .filter(
-              (ques) => ques.type === "checkbox" || ques.type === "radiogroup"
-            )
-            .map((ques) => {
-              return (
-                <li key={ques.name}>
-                  {ques.name}- {ques.title}
-                  <Button onClick={() => removeQuestion(ques.name)}>
-                    Remove me
-                  </Button>
-                </li>
-              );
-            })}
-      </ul>
+      <CurrentQuestions questions={questions} removeQuestion={removeQuestion} />
     </>
   );
 };
