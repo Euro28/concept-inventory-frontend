40c5b01cb17562631fe2aae06570df46cb1d2d37
Euro28 <eurol197628@gmail.com>
2021-01-18 03:22:45 +0000

    result page

--- src/Quiz/results.jsx
+++ src/Quiz/results.jsx
@@ -1,37 +1,68 @@
 import axios from "axios";
+import array from "lodash/array";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+
+import Spinner from "./Spinner.jsx";
+import ConceptResult from "./ConceptResult.jsx";
 
 const Results = () => {
-  const [quizResults, setQuizResults] = useState({});
+  const [quizResults, setQuizResults] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const param = useParams();
+  const sameArr = (correct, given) => {
+    const diff = array.intersection(correct, given);
+    return diff.length === correct.length && correct.length === given.length;
+  };
 
   useEffect(() => {
     const fetchResults = async () => {
       try {
         setLoading(true);
         const results = await axios.get("/api/results");
-        setQuizResults(results.data); //im gonna pass this results to some other component which renders it prettier
+        const questions = await axios.get("/api/questions");
+
+        const correct = questions.data[0].pages[0].elements.map((question) => ({
+          misconception: question.misconception,
+          correct: sameArr(question.correctAnswer, results[question.valueName]),
+        }));
+
+        const concepts = array.uniq(correct.map((ques) => ques.misconception));
+        const count = {};
+
+        concepts.forEach(
+          (concept) => (count[concept] = { total: 0, correct: 0 })
+        );
+
+        correct.forEach((ans) => {
+          count[ans.misconception].total++;
+          if (ans.correct) count[ans.misconception].correct++;
+        });
+
+        console.log(count);
+        setQuizResults(count);
         setLoading(false);
       } catch (err) {
-        console.log(err);
+        setError(err);
       }
     };
 
     fetchResults();
-  }, [param.id]);
+  }, []);
 
-  return (
-    <>
-      {loading ? (
-        <div> Loading... (spinner goes here) </div>
-      ) : (
-        JSON.stringify(quizResults)
-      )}
-    </>
+  const results = (
+    <div>
+      {Object.keys(quizResults).map((concept) => (
+        <ConceptResult
+          key={concept}
+          title={concept}
+          score={quizResults[concept]}
+        />
+      ))}
+    </div>
   );
+
+  return <>{loading ? <Spinner /> : results}</>;
 };
 
 export default Results;
